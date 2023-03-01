7ddade9069d82db6034d44f32f45265914c279fe
StarHawkMK1 <118034465+StarHawkMK1@users.noreply.github.com>
2023-03-01 16:47:25 +0900

    Add files via upload

---
+++ Ch1/Example/Ex1_5_JudgeABC2.java
@@ -0,0 +1,20 @@
+package DoIt.JavaAlgorithm.Ch1;
+
+import java.util.Scanner;
+
+public class Ex1_5_JudgeABC2 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+        System.out.print("정수를 입력하세요.: ");
+        int n = sc.nextInt();
+
+        if (n == 1) {
+            System.out.println("A");
+        } else if (n == 2) {
+            System.out.println("B");
+        } else if (n == 3) {
+            System.out.println("C");
+        } else;
+    }
+}
